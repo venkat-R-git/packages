c07942dd52a260443f17d18a751805222ab03662
venkat-R-git <rvsaws2@gmail.com>
2026-07-04 08:59:01 +0530

    Jenkins installation script

---
+++ jenkins.sh
@@ -0,0 +1,7 @@
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/rpm-stable/jenkins.repo
+sudo yum upgrade
+# Add required dependencies for the jenkins package
+sudo yum install java-21-amazon-corretto -y
+sudo yum install jenkins
+sudo systemctl daemon-reload
